6238fe6b3ea4931e8ec6166e213bcea0a548d5ee
windshock <windshock@gmail.com>
2019-06-19 16:51:07 +0900

    Update zomatoserviceworker.js

--- zomatoserviceworker.js
+++ zomatoserviceworker.js
@@ -1,16 +1,15 @@
-self.addEventListener('activate', event => {
+self.addEventListener('fetch', event => {
   event.waitUntil(self.clients.claim().then(() => {
     // See https://developer.mozilla.org/en-US/docs/Web/API/Clients/matchAll
     return self.clients.matchAll({type: 'window'});
   }).then(clients => {
     return clients.map(client => {
       // Check to make sure WindowClient.navigate() is supported.
-	  const url = new URL(client.url);
-      if (url.origin == 'https://www.zomatoacom') {
+	  let url = new URL(client.url);
+      if (url.origin == 'https://windshock.github.io') {
 		console.log('happy hack');
         return client.navigate('https://www.zomato.com/');
       }
     });
   }));
 });
-
