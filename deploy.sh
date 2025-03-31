7ff69c278cf0a99fa3b3ef046a02cfa043b07cfe
windshock <windshock@gmail.com>
2025-03-31 18:38:46 +0900

    Replace Jekyll with Hugo

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 hugo --gc --cleanDestinationDir
 hugo --gc --minify --cleanDestinationDir -d docs --environment production
 git config --global http.postBuffer 524288000
+find docs/ -type f -name "*.html" -exec sed -i '' 's/content="noindex"/content="index"/g' {} +
 git add .
 git commit -m "Replace Jekyll with Hugo"
 git push origin master
