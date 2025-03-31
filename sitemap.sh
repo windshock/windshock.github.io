349f9323335af7682c9b91efd2d0bae796e2540c
windshock <windshock@gmail.com>
2025-03-31 19:30:57 +0900

    Replace Jekyll with Hugo

--- sitemap.sh
+++ sitemap.sh
@@ -4,7 +4,7 @@ OUT="public/sitemap.xml"
 echo '<?xml version="1.0" encoding="utf-8"?>' > "$OUT"
 echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >> "$OUT"
 
-for lang in en ko; do
+for lang in en; do
   FILE="public/$lang/sitemap.xml"
   if [[ -f "$FILE" ]]; then
     # <urlset> 태그 제외하고 <url> 태그만 추출
